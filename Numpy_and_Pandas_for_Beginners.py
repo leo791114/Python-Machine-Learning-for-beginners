cdc0cd187ed389e3df3e2388216955ffe13ee872
leo791114 <leo791114@hotmail.com>
2017-08-17 20:52:29 +0800

    Python pandas groupby

--- Numpy_and_Pandas_for_Beginners.py
+++ Numpy_and_Pandas_for_Beginners.py
@@ -3,17 +3,17 @@
 
 #  <h1 style="color: red">Numpy for Beginners</h1>
 
-# In[22]:
+# In[3]:
 
 import numpy as np  #Import numpy
 
 
-# In[23]:
+# In[4]:
 
 array = np.array([[1,2,3],[4,5,6]])  #Create a two-dimensional array
 
 
-# In[24]:
+# In[5]:
 
 print(array)
 print('Dim', array.ndim)
@@ -21,7 +21,7 @@ print('Shape', array.shape)
 print('Size', array.size)
 
 
-# In[25]:
+# In[6]:
 
 a = np.array([1,2,3,4,5])
 b = np.arange(2,14,2).reshape((2,3))#Create an array from two to twelve, not included, with step of two
@@ -49,7 +49,7 @@ print(a,b,c,d,e,sep='\n')
 #print(f==5)
 
 
-# In[26]:
+# In[7]:
 
 a = np.array([[3,6,9],[12,15,18]])
 print(a)
@@ -58,7 +58,7 @@ print(np.sum(a, axis=1)) #add all elements on the basis of row
 print(np.sum(a, axis=0)) #add all elements on the basis of column
 
 
-# In[27]:
+# In[8]:
 
 a = np.array([[2,4,6],[8,10,12]])
 b = np.arange(6).reshape((2,3))
@@ -68,7 +68,7 @@ print(b.T) #transpose
 print(np.dot(a,b.T)) # This is array multiplication
 
 
-# In[28]:
+# In[9]:
 
 #Merge
 A = np.array([1,1,1])
@@ -77,7 +77,7 @@ print(np.vstack((A,B))) #vertical merge
 print(np.hstack((A,B))) #horizontal merge
 
 
-# In[29]:
+# In[10]:
 
 #Split
 a = np.arange(12).reshape((3,4))
@@ -90,7 +90,7 @@ print(np.hsplit(a,2)) #Horizontally split the array into 2 new arrays
 
 # <h3 style="color:blue">Series</h3>
 
-# In[30]:
+# In[11]:
 
 import pandas as pd
 s = pd.Series([1,'abc','6',np.nan,44,1])
@@ -102,7 +102,7 @@ print(a)
 
 #  <h3 style="color: blue">DataFrame</h3> 
 
-# In[31]:
+# In[12]:
 
 #Method one: use array to create DataFrame
 print(np.random.random()) # np.random.random returns random floats in the half-open interval [0 1)
@@ -113,7 +113,7 @@ df = pd.DataFrame(np.random.randn(7,3))
 print(df)
 
 
-# In[32]:
+# In[13]:
 
 #Method one: use array to create DataFrame
 eat = np.random.randint(10, size=(7,3))*5+50
@@ -127,7 +127,7 @@ df1 = pd.DataFrame(eat, index=dates, columns=['Breakfast', 'Lunch', 'Dinner'])
 print(df1)
 
 
-# In[33]:
+# In[14]:
 
 #Method two: use dictionary to create DataFrame
 df2 = pd.DataFrame({'Decimal': pd.Series([1,3,6,4], index=list(range(4)), dtype='float32'),
@@ -152,7 +152,7 @@ print(df2.sort_index(axis=1,ascending=False)) #axis=1 takes columns as sorting i
 print(df2.sort_values(by='Decimal'))
 
 
-# In[34]:
+# In[15]:
 
 # Pandas: Selecting data
 print(df1)
@@ -162,7 +162,7 @@ print('\n')
 print(df1[0:3])
 
 
-# In[35]:
+# In[16]:
 
 print(df1)
 # Pandas: Selecting data by label (refer to the label and not the position)
@@ -177,13 +177,13 @@ print('\n')
 print(df1.ix[:3,['Lunch','Dinner']])
 
 
-# In[36]:
+# In[17]:
 
 # Pandas: Conditional expression
 print(df1[df1.Lunch > 80])
 
 
-# In[37]:
+# In[18]:
 
 # Pandas: Re-assign data
 #df1.Dinner[df1.Lunch>80]=40  --> this method Pandas would select df1.Dinner first, and then returns
@@ -196,20 +196,20 @@ print(df1)
 print(df1.dtypes)
 
 
-# In[38]:
+# In[19]:
 
 # Pandas: Dealing with missing values
 print(df1.isnull())
 df1.isnull().sum()
 
 
-# In[39]:
+# In[20]:
 
 # Panadas: Dealing with missing values --> replace them with 0
 df1.fillna(value=0)
 
 
-# In[40]:
+# In[21]:
 
 # Panadas: Dealing with missing values --> erase the missing values
 print(df1)
@@ -220,7 +220,7 @@ df1.dropna(
 )
 
 
-# In[41]:
+# In[22]:
 
 # Pandas: Other usages
 print(df1)
@@ -228,16 +228,75 @@ df1.dropna(thresh=3)  #Keep only the rows with at least 3 non-na values
 df1.dropna(subset=['Breakfast']) #Only drops the rows or columns in the subset that have NaN value.
 
 
-# In[54]:
+# In[29]:
 
 # Pandas: Dealing with missing values --> interpolation
 from sklearn.preprocessing import Imputer
-print(df1.dtypes)
+# print(df1.dtypes)
 print(df1.values)
 print(df1.values.dtype)
 # print(df1.values.dtypes)
-imr = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imr = Imputer(missing_values='NaN', strategy='mean', axis=0) #axis = 0 for columns, axis = 1 for rows
+imr_2 = Imputer(missing_values='NaN', strategy='mean', axis=0)
 imr = imr.fit(df1)
 imputed_data = imr.transform(df1.values)
-imputed_data
+imputed_data_2 = imr_2.fit_transform(df1) #fit + transform
+print(imputed_data)
+print(imputed_data_2)
+
+
+# In[37]:
+
+# Pandas: load file
+
+#load csv file
+data = pd.read_csv('nescsv2.csv')
+print(data)
+data = data.T
+print(data)
+data.to_csv('nescsv2_transpose.csv')
+
+
+# In[78]:
+
+# Pandas merge DataFrame
+
+#concat
+
+df1_merge = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'])
+df2_merge = pd.DataFrame(np.ones((3,4))*1, columns=['a','b','c','d'])
+df3_merge = pd.DataFrame(np.ones((3,4))*2, columns=['a','b','c','d'])
+# print(df1_merge, df2_merge, df3_merge,sep='\n')
+res = pd.concat([df1_merge, df2_merge, df3_merge], axis = 0, ignore_index = True)
+# print('\n')
+# print(res)
+# print('\n')
+print(df1_merge.columns)
+print(df1_merge.index)
+#join: {'inner', 'outer'}
+df4_merge = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'],index=[1,2,3])
+df5_merge = pd.DataFrame(np.ones((3,4))*1, columns=['b','c','d','e'],index=[2,3,4])
+res_outer = pd.concat([df4_merge, df5_merge], axis=0, join='outer', ignore_index=False)
+res_inner = pd.concat([df4_merge, df5_merge], axis=0, join='inner', ignore_index=False)
+res_reset = res_inner.reset_index(drop=True)
+print(res_reset)
+print(res_reset.groupby(['b','c']))
+# print(res_outer, res_inner, sep='\n')
+# print(diff_inner_outer)
+
+
+# In[88]:
+
+df_test = pd.DataFrame({'A' : ['foo', 'bar', 'foo', 'bar',
+                              'foo', 'bar', 'foo', 'foo'],
+                        'B' : ['one', 'one', 'two', 'three',
+                              'two', 'two', 'one', 'three'],
+                        'C' : np.random.randn(8),
+                        'D' : np.random.randn(8)})
+df_test
+grouped = df_test.groupby(['A','B'])
+print(grouped.count())
+# print(grouped)
+# print(grouped.groups)
+print(df_test)
 
