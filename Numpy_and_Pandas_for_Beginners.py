e9af6d7a46453ada01cef0ccad111c9aa470366a
leo791114 <leo791114@hotmail.com>
2017-08-18 01:58:48 +0800

    Comparing two dataframes and getting the difference

--- Numpy_and_Pandas_for_Beginners.py
+++ Numpy_and_Pandas_for_Beginners.py
@@ -3,17 +3,17 @@
 
 #  <h1 style="color: red">Numpy for Beginners</h1>
 
-# In[3]:
+# In[2]:
 
 import numpy as np  #Import numpy
 
 
-# In[4]:
+# In[3]:
 
 array = np.array([[1,2,3],[4,5,6]])  #Create a two-dimensional array
 
 
-# In[5]:
+# In[4]:
 
 print(array)
 print('Dim', array.ndim)
@@ -21,7 +21,7 @@ print('Shape', array.shape)
 print('Size', array.size)
 
 
-# In[6]:
+# In[5]:
 
 a = np.array([1,2,3,4,5])
 b = np.arange(2,14,2).reshape((2,3))#Create an array from two to twelve, not included, with step of two
@@ -49,7 +49,7 @@ print(a,b,c,d,e,sep='\n')
 #print(f==5)
 
 
-# In[7]:
+# In[6]:
 
 a = np.array([[3,6,9],[12,15,18]])
 print(a)
@@ -58,7 +58,7 @@ print(np.sum(a, axis=1)) #add all elements on the basis of row
 print(np.sum(a, axis=0)) #add all elements on the basis of column
 
 
-# In[8]:
+# In[7]:
 
 a = np.array([[2,4,6],[8,10,12]])
 b = np.arange(6).reshape((2,3))
@@ -68,7 +68,7 @@ print(b.T) #transpose
 print(np.dot(a,b.T)) # This is array multiplication
 
 
-# In[9]:
+# In[8]:
 
 #Merge
 A = np.array([1,1,1])
@@ -77,7 +77,7 @@ print(np.vstack((A,B))) #vertical merge
 print(np.hstack((A,B))) #horizontal merge
 
 
-# In[10]:
+# In[9]:
 
 #Split
 a = np.arange(12).reshape((3,4))
@@ -90,7 +90,7 @@ print(np.hsplit(a,2)) #Horizontally split the array into 2 new arrays
 
 # <h3 style="color:blue">Series</h3>
 
-# In[11]:
+# In[10]:
 
 import pandas as pd
 s = pd.Series([1,'abc','6',np.nan,44,1])
@@ -102,7 +102,7 @@ print(a)
 
 #  <h3 style="color: blue">DataFrame</h3> 
 
-# In[12]:
+# In[11]:
 
 #Method one: use array to create DataFrame
 print(np.random.random()) # np.random.random returns random floats in the half-open interval [0 1)
@@ -113,7 +113,7 @@ df = pd.DataFrame(np.random.randn(7,3))
 print(df)
 
 
-# In[13]:
+# In[12]:
 
 #Method one: use array to create DataFrame
 eat = np.random.randint(10, size=(7,3))*5+50
@@ -127,7 +127,7 @@ df1 = pd.DataFrame(eat, index=dates, columns=['Breakfast', 'Lunch', 'Dinner'])
 print(df1)
 
 
-# In[14]:
+# In[35]:
 
 #Method two: use dictionary to create DataFrame
 df2 = pd.DataFrame({'Decimal': pd.Series([1,3,6,4], index=list(range(4)), dtype='float32'),
@@ -135,14 +135,14 @@ df2 = pd.DataFrame({'Decimal': pd.Series([1,3,6,4], index=list(range(4)), dtype=
                    'Time': pd.Timestamp('20170812'),
                    'Category': pd.Categorical(['test','train','test','train'])})
 x = pd.Series(1,index=list(range(4)), dtype='float32')
-#print(x)
+# print(x)
 y = np.array([3]*4, dtype='int32')
 #print(y)
 z = pd.Timestamp('20170816')
 #print(z)
 k = pd.Categorical(['test','train','test','train'])
-#print(k)
-#print(df2)
+# print(k)
+print(df2)
 #print(df2.dtypes)
 #print(df2.index)
 #print(df2.columns)
@@ -152,7 +152,7 @@ print(df2.sort_index(axis=1,ascending=False)) #axis=1 takes columns as sorting i
 print(df2.sort_values(by='Decimal'))
 
 
-# In[15]:
+# In[14]:
 
 # Pandas: Selecting data
 print(df1)
@@ -162,7 +162,7 @@ print('\n')
 print(df1[0:3])
 
 
-# In[16]:
+# In[15]:
 
 print(df1)
 # Pandas: Selecting data by label (refer to the label and not the position)
@@ -177,13 +177,13 @@ print('\n')
 print(df1.ix[:3,['Lunch','Dinner']])
 
 
-# In[17]:
+# In[16]:
 
 # Pandas: Conditional expression
 print(df1[df1.Lunch > 80])
 
 
-# In[18]:
+# In[17]:
 
 # Pandas: Re-assign data
 #df1.Dinner[df1.Lunch>80]=40  --> this method Pandas would select df1.Dinner first, and then returns
@@ -196,20 +196,20 @@ print(df1)
 print(df1.dtypes)
 
 
-# In[19]:
+# In[18]:
 
 # Pandas: Dealing with missing values
 print(df1.isnull())
 df1.isnull().sum()
 
 
-# In[20]:
+# In[19]:
 
 # Panadas: Dealing with missing values --> replace them with 0
 df1.fillna(value=0)
 
 
-# In[21]:
+# In[20]:
 
 # Panadas: Dealing with missing values --> erase the missing values
 print(df1)
@@ -220,7 +220,7 @@ df1.dropna(
 )
 
 
-# In[22]:
+# In[21]:
 
 # Pandas: Other usages
 print(df1)
@@ -228,7 +228,7 @@ df1.dropna(thresh=3)  #Keep only the rows with at least 3 non-na values
 df1.dropna(subset=['Breakfast']) #Only drops the rows or columns in the subset that have NaN value.
 
 
-# In[29]:
+# In[22]:
 
 # Pandas: Dealing with missing values --> interpolation
 from sklearn.preprocessing import Imputer
@@ -245,7 +245,7 @@ print(imputed_data)
 print(imputed_data_2)
 
 
-# In[37]:
+# In[23]:
 
 # Pandas: load file
 
@@ -257,7 +257,7 @@ print(data)
 data.to_csv('nescsv2_transpose.csv')
 
 
-# In[78]:
+# In[29]:
 
 # Pandas merge DataFrame
 
@@ -278,25 +278,56 @@ df4_merge = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'],index=[1,2,
 df5_merge = pd.DataFrame(np.ones((3,4))*1, columns=['b','c','d','e'],index=[2,3,4])
 res_outer = pd.concat([df4_merge, df5_merge], axis=0, join='outer', ignore_index=False)
 res_inner = pd.concat([df4_merge, df5_merge], axis=0, join='inner', ignore_index=False)
+print(res_outer)
 res_reset = res_inner.reset_index(drop=True)
+print(list(res_reset.columns))
 print(res_reset)
 print(res_reset.groupby(['b','c']))
 # print(res_outer, res_inner, sep='\n')
 # print(diff_inner_outer)
 
 
-# In[88]:
+# In[105]:
 
-df_test = pd.DataFrame({'A' : ['foo', 'bar', 'foo', 'bar',
-                              'foo', 'bar', 'foo', 'foo'],
-                        'B' : ['one', 'one', 'two', 'three',
-                              'two', 'two', 'one', 'three'],
-                        'C' : np.random.randn(8),
-                        'D' : np.random.randn(8)})
-df_test
-grouped = df_test.groupby(['A','B'])
-print(grouped.count())
-# print(grouped)
-# print(grouped.groups)
-print(df_test)
+df1_test = pd.DataFrame({
+    'Date': pd.Timestamp('20170812'),
+    'Fruit': ['Banana', 'Orange', 'Apple', 'Celery'],
+    'Num': [22.1,8.6,7.6,10.2],
+    'Color': ['Yello', 'Orange', 'Green', 'Green']
+})
+
+df2_test = pd.DataFrame({
+    'Fruit': ['Banana', 'Orange', 'Apple', 'Celery', 'Apple', 'Orange'],
+    'Num': [22.1, 8.6, 7.6, 10.2, 22.1, 8.6],
+    'Color': ['Yellow','Orange','Green','Green','Red','Orange']
+})
+dates_test = []
+for i in range(1,7):
+    if i <=4:
+        dates_test.append(pd.Timestamp('20170812'))
+    else:
+        dates_test.append(pd.Timestamp('20170813')) 
+
+df2_test.insert(0, 'Date', dates_test)
+# print(dates_test)
+print(df2_test)
+print(df1_test)
+
+df_merge_test = pd.concat([df1_test, df2_test])
+print(df_merge_test)
+df_merge_test = df_merge_test.reset_index(drop = True) #pd.concat([df1_test, df2_test], ignore_index=True) could get the same effect
+print(df_merge_test)
+df_gpby = df_merge_test.groupby(list(df_merge_test))
+# print(df_gpby.groups)
+print(df_gpby.groups.values())
+# print(df_gpby.count())
+idx = [x[0] for x in df_gpby.groups.values() if len(x) == 1]
+print(idx)
+
+df_merge_test.reindex(idx)
+
+
+# In[ ]:
+
+
 
