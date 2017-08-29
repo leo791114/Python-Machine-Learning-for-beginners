add2731a8d16d57346bb26961a504f80c26e0355
leo791114 <leo791114@hotmail.com>
2017-08-29 19:07:02 +0800

    Scikit-learn Iris

--- Python_Scikit-learn.py
+++ Python_Scikit-learn.py
@@ -1,7 +1,34 @@
 
 # coding: utf-8
 
+# <h3 style="color: blue">KNN Classifier - IRIS</h3>
+# <h4 style="color: red">Processes:</h4>
+# <ol>
+#     <li>
+#         <b>Import module:</b> 
+#         <ul><li>datasets, train_test_split, KNeighborsClassifier</li></ul>
+#     </li>
+#     <li>
+#         <b>Create Data:</b>
+#         <ul>
+#             <li>classes, labels</li>
+#             <li>Data set --> training set/data, testing set/data</li>
+#         </ul>
+#     </li>
+#     <li>
+#         <b>Create Model:</b>
+#         <ul>
+#             <li>fit, predict</li>
+#         </ul>
+#     </li>
+#     <li>
+#         <b>Visualization</b>
+#     </li>
+# </ol>
+
 # In[ ]:
 
-
+import numpy as np
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
 
