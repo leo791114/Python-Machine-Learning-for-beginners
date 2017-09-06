892a0028965ce90687a425c6156134b8f570bdab
leo791114 <leo791114@hotmail.com>
2017-09-06 18:52:37 +0800

    K-means_practice

--- K-means_practice.py
+++ K-means_practice.py
@@ -5,5 +5,21 @@ import numpy as np
 import pandas as pd
 
 #%%
-df = pd.read_csv('data_1024.csv')
-print(df)
+
+data_csv = pd.read_csv('data1024.csv')
+df_col = data_csv.columns[0].split('\t')
+
+print(data_csv.values.tolist())
+print(type(data_csv.values.tolist()))
+print(df_col)
+
+# print(data_csv)
+# print(type(data_csv))
+
+
+
+
+
+
+
+
