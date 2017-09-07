b7361af54e12b9b1979434645680560fd3c2da81
leo791114 <leo791114@hotmail.com>
2017-09-07 19:17:32 +0800

    K-means_practice grid style

--- K-means_practice.py
+++ K-means_practice.py
@@ -1,20 +1,49 @@
 #%%
 from sklearn import datasets
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import matplotlib.axes  as ax
 import numpy as np
 import pandas as pd
 
 #%%
 
 data_csv = pd.read_csv('data1024.csv')
-df_col = data_csv.columns[0].split('\t')
+data_col = data_csv.columns[0].split('\t')
+data_val = data_csv.values.tolist()
+df_val = []
 
-print(data_csv.values.tolist())
-print(type(data_csv.values.tolist()))
-print(df_col)
+#%%
 
-# print(data_csv)
-# print(type(data_csv))
+#Split df_val element
+for ele in data_val:
+    idx = data_val.index(ele)
+    df_val.append(ele[0].split('\t'))
+
+print (df_val)
+
+#%%
+#Create DataFrame
+df = pd.DataFrame(df_val, columns=df_col)
+
+print(type(df))
+print(df.columns)
+print(df.index)
+
+#Draw scatter plot of the dataframe of input 
+fig = plt.figure()
+
+plt.scatter(df[df.columns[1]], df[df.columns[2]], c='black')
+plt.xlabel(df.columns[1])
+plt.ylabel(df.columns[2])
+print(plt.plot())
+plt.grid(color='c', linestyle=':')
+plt.show()
+
+
+# print(df_val)
+# print(type(data_csv.values.tolist()))
+# print(df_col)
 
 
 
