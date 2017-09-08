9d80096ae201ae2ad4c3ddccd9eefed48b6046a4
leo791114 <leo791114@hotmail.com>
2017-09-08 18:37:04 +0800

    Set background color for subplot

--- K-means_practice.py
+++ K-means_practice.py
@@ -24,22 +24,31 @@ print (df_val)
 
 #%%
 #Create DataFrame
-df = pd.DataFrame(df_val, columns=df_col)
+df = pd.DataFrame(df_val, columns=data_col)
 
 print(type(df))
 print(df.columns)
 print(df.index)
 
+#%%
 #Draw scatter plot of the dataframe of input 
-fig = plt.figure()
 
+fig1 = plt.figure(1)  #create a figure instance
+ax = fig1.gca()  #get the current axes
+ax.grid(color='c', linestyle=':')  #draw the grid 
+ax.set_axisbelow(True)  #put the grid behind the graph
+ax.set_axis_bgcolor('grey')
 plt.scatter(df[df.columns[1]], df[df.columns[2]], c='black')
 plt.xlabel(df.columns[1])
 plt.ylabel(df.columns[2])
-print(plt.plot())
-plt.grid(color='c', linestyle=':')
 plt.show()
 
+
+#%%
+f = plt.figure(1)
+
+#%%
+f.axes
 
 # print(df_val)
 # print(type(data_csv.values.tolist()))
