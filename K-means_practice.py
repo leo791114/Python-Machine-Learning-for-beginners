d3eb970dee4e319709af2f55e995fed18fd15268
leo791114 <leo791114@hotmail.com>
2017-09-12 01:19:55 +0800

    Finish K-means_practice

--- K-means_practice.py
+++ K-means_practice.py
@@ -2,7 +2,7 @@
 from sklearn import datasets
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-import matplotlib.axes  as ax
+import matplotlib.axes as ax
 import numpy as np
 import pandas as pd
 
@@ -15,15 +15,15 @@ df_val = []
 
 #%%
 
-#Split df_val element
+# Split df_val element
 for ele in data_val:
     idx = data_val.index(ele)
     df_val.append(ele[0].split('\t'))
 
-print (df_val)
+print(df_val)
 
 #%%
-#Create DataFrame
+# Create DataFrame
 df = pd.DataFrame(df_val, columns=data_col)
 
 print(type(df))
@@ -31,33 +31,14 @@ print(df.columns)
 print(df.index)
 
 #%%
-#Draw scatter plot of the dataframe of input 
+# Draw scatter plot of the dataframe of input
 
-fig1 = plt.figure(1)  #create a figure instance
-ax = fig1.gca()  #get the current axes
-ax.grid(color='c', linestyle=':')  #draw the grid 
-ax.set_axisbelow(True)  #put the grid behind the graph
-ax.set_axis_bgcolor('grey')
+fig1 = plt.figure(1)  # create a figure instance
+ax = fig1.gca()  # get the current axes
+ax.grid(color='c', linestyle=':')  # draw the grid
+ax.set_axisbelow(True)  # put the grid behind the graph
+ax.set_axis_bgcolor('whitesmoke')
 plt.scatter(df[df.columns[1]], df[df.columns[2]], c='black')
 plt.xlabel(df.columns[1])
 plt.ylabel(df.columns[2])
 plt.show()
-
-
-#%%
-f = plt.figure(1)
-
-#%%
-f.axes
-
-# print(df_val)
-# print(type(data_csv.values.tolist()))
-# print(df_col)
-
-
-
-
-
-
-
-
