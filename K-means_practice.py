3567516686f8d5378836c64c1c06e89f8b32a80c
leo791114 <leo791114@hotmail.com>
2017-09-13 01:46:06 +0800

    Try to draw graph out of kmeans result-Kmeans_practice

--- K-means_practice.py
+++ K-means_practice.py
@@ -42,3 +42,24 @@ plt.scatter(df[df.columns[1]], df[df.columns[2]], c='black')
 plt.xlabel(df.columns[1])
 plt.ylabel(df.columns[2])
 plt.show()
+
+#%%
+# Choose K and run the algorithm
+f1 = df['Distance_Feature'].values
+f2 = df['Speeding_Feature'].values
+X = zip(f1, f2)
+
+# Converting iterator to list
+X_list = list(X)
+print(X_list)
+
+# Converting iterator to set
+# X_set = set(X)
+# print(X_set)
+
+# Converting iterator to Numpy matrix
+# X_matrix = np.matrix(X)
+kmeans = KMeans(n_clusters=2).fit(X_list)
+kmeans
+kmeans.labels_
+kmeans.cluster_centers_
