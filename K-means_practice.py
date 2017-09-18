c2a41c472b37ddbdf3ec991607c620e5db6b4dc9
leo791114 <leo791114@hotmail.com>
2017-09-19 02:13:26 +0800

    Finish K=2 K-means practice

--- K-means_practice.py
+++ K-means_practice.py
@@ -53,8 +53,8 @@ print(X)
 
 #%%
 # Converting iterator to list
-# X_list = list(X)
-# print(X_list)
+X_list = list(X)
+print(X_list)
 
 #%%
 # Converting iterator to set
@@ -62,21 +62,56 @@ print(X)
 # print(X_set)
 
 #%%
-#Converting iterator to matrix
-X_matrix = np.matrix(X)
+# Converting iterator to matrix
+'''
+Here, np.matrix can't sipply accept zip(f1, f2) because in Python 3, zip function returns
+iterator rather than list or tuple which would raise typeerror.
+'''
+X_matrix = np.matrix(X_list)
 print(X_matrix)
 print(type(X_matrix))
 
 #%%
-# Converting iterator to Numpy matrix
-# X_matrix = np.matrix(X)
-kmeans = KMeans(n_clusters=2).fit(X_list)
+# Kmeans clustering
+kmeans = KMeans(n_clusters=2).fit(X_matrix)
 kmeans
-kmeans.labels_
-kmeans.cluster_centers_
+print(kmeans.labels_)
+print(kmeans.cluster_centers_)
+df.insert(0, 'label', kmeans.labels_)
+df_group_1 = df[df['label'] == 0]
+df_group_2 = df[df['label'] == 1]
+centers = kmeans.cluster_centers_
 
 
-#Reference
+#%%
+# Draw scatter plot of the kmeans data
+# Create a reference Axes and keep drawing on the same subplot
+fig2 = plt.figure(2)
+ax2 = fig2.gca()
+ax2.grid(color='c', linestyle=':')
+ax2.set_axisbelow(True)
+ax2.set_axis_bgcolor('gainsboro')
+ax2.scatter(df_group_1[df_group_1.columns[2]], df_group_1[df_group_1.columns[3]],
+            c='green', label='Group 1')  # label attribute could assign legend
+ax2.scatter(df_group_2[df_group_2.columns[2]], df_group_2[df_group_2.columns[3]],
+            c='blue', label='Group 2')  # label attribute could assign legend
+'''
+Draw the centroids.
+edgecolors: the outlines of the markers
+facecolors: the string 'none' to create a hollow marker
+'''
+ax2.scatter(centers[0][0], centers[0][1], edgecolors='black',
+            marker=(5, 1, 0), s=500, facecolors='none')
+ax2.scatter(centers[1][0], centers[1][1], edgecolors='black',
+            marker=(5, 1, 0), s=500, facecolors='none')
+ax2.legend()  # Add legend
+plt.xlabel(df.columns[2])
+plt.ylabel(df.columns[3])
+fig2.set_size_inches(8, 8)
+plt.show()
+
+
+# Reference
 # https://stackoverflow.com/questions/231767/what-does-the-yield-keyword-do
 # http://blog.blackwhite.tw/2013/05/python-yield-generator.html
 # https://stackoverflow.com/questions/31683959/the-zip-function-in-python-3
